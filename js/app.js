1d4a6d99e290fd2e00e27c7dd134eea6e86285a3
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2025-10-20 16:14:06 +0200

    ajout de la photo

--- js/app.js
+++ js/app.js
@@ -1,29 +1,36 @@
-//console.log("Bonjour");
+
+//console.log('ok');
 const API_URL = 'https://cruth.phpnet.org/epfc/caviste/public/index.php/api';
+const PHOTOS_URL = 'https://cruth.phpnet.org/epfc/caviste/public/pics/';
 
-fetch('https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines')
-    .then(response => response.json())
-    .then(json => {
+fetch(API_URL + '/wines')
+.then(response => response.json())
+.then(data => {
+    const ULWineList = document.querySelector('#wineList');
+    ULWineList.innerHTML = '';
 
-        const ULWineList = document.querySelector('#wineList');
-        ULWineList.innerHTML ="";
-        json.forEach(wine => {
-            console.log(wine.name);
-            let LI = document.createElement('li');
-            LI.innerHTML = wine.name;
-            LI.classList.add('list-groupep-item');
-            LI.dataset.wineId = wine.id;
-            LI.addEventListener('click', function (e) {
+    data.forEach(wine => {
+        console.log(wine.name);
 
-                let wineId = LI.dataset.wineId;
-                let results = data.filter(wine == wine.id);
-                let wine = results.length > 0 ? results[0] : undefined;
-                console.log(wine);
+        //ULWineList.innerHTML += `<li data-wine-id="${wine.id}" class="list-group-item" onclick="....">${wine.name}</li>`   //Solution 1
+        let LI = document.createElement('li');              //Solution 2
+        LI.innerHTML = wine.name;
+        LI.classList.add('list-group-item');
+        LI.dataset.wineId = wine.id;
 
-            });
+        LI.addEventListener('click', function(e) {
+            let wineId = LI.dataset.wineId;
 
-            ULWineList.appendChild(LI);
+            let results = data.filter(wine => wineId==wine.id);
+            
+            let wine = results.length>0 ? results[0] : undefined;
 
+            console.log(wine);
+            //Affichage de la photo du vin
+            const photo = document.querySelector('#photo');
+            photo.src = PHOTOS_URL + wine.picture;
         });
 
+        ULWineList.appendChild(LI);
     });
+})
